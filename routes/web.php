1d9c9fb943f170139d6038a2e823a6173aa98a0f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-18 23:32:29 +0000

    Remove concept nav, make G the homepage, reposition main nav

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('concepts.a'));
-Route::get('/concept-b', fn() => view('concepts.b'));
-Route::get('/concept-c', fn() => view('concepts.c'));
-Route::get('/concept-d', fn() => view('concepts.d'));
-Route::get('/concept-e', fn() => view('concepts.e'));
-Route::get('/concept-f', fn() => view('concepts.f'));
-Route::get('/concept-g', fn() => view('concepts.g'));
+Route::get('/', fn() => view('concepts.g'));
