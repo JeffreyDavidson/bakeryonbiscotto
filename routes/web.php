ae21c59edb37720f467892704b65e67e239574a8
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-19 04:59:24 +0000

    Add 3 gallery concept options for Fresh from the Oven

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('concepts.g'));
 Route::get('/menu-concepts', fn() => view('menu-concepts'));
+Route::get('/gallery-concepts', fn() => view('gallery-concepts'));
 Route::get('/upgrades', fn() => view('upgrades'));
 Route::get('/wow', fn() => view('wow'));
