fa2e0bef1774deac85bbadc55541b6c3f853126e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-19 05:25:21 +0000

    Add 5 kitchen-focused photo gallery concepts (N-R), no social CTAs

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::get('/menu-concepts', fn() => view('menu-concepts'));
 Route::get('/gallery-concepts', fn() => view('gallery-concepts'));
 Route::get('/gallery-concepts-2', fn() => view('gallery-concepts-2'));
 Route::get('/gallery-concepts-3', fn() => view('gallery-concepts-3'));
+Route::get('/gallery-concepts-4', fn() => view('gallery-concepts-4'));
 Route::get('/upgrades', fn() => view('upgrades'));
 Route::get('/wow', fn() => view('wow'));
