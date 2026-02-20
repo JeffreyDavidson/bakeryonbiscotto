89041addb396a2a31acdde9a93e8bdc47d492bf0
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-20 15:26:07 +0000

    Gallery finals: enhanced flour table + cooling rack side by side

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ Route::get('/gallery-concepts-2', fn() => view('gallery-concepts-2'));
 Route::get('/gallery-concepts-3', fn() => view('gallery-concepts-3'));
 Route::get('/gallery-concepts-4', fn() => view('gallery-concepts-4'));
 Route::get('/gallery-concepts-5', fn() => view('gallery-concepts-5'));
+Route::get('/gallery-finals', fn() => view('gallery-finals'));
 Route::get('/upgrades', fn() => view('upgrades'));
 Route::get('/wow', fn() => view('wow'));
