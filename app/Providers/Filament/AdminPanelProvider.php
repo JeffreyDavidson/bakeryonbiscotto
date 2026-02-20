05c7a3121e6ffc87353d61f42524a31539472030
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-20 17:48:28 +0000

    Remove MaxWidth enum, use string

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,7 +9,6 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Support\Enums\MaxWidth;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -31,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogo(asset('images/logo.jpg'))
             ->brandLogoHeight('60px')
             ->darkMode(false)
-            ->maxContentWidth(MaxWidth::Full)
+            ->maxContentWidth('full')
             ->colors([
                 'primary' => [
                     50 => '253, 248, 242',
