3f6a09c21e1df6a5bcdd3d952eff8ae8ef09bfee
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-21 18:45:50 +0000

    Use hero banner image in email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/logo.jpg') }}" alt="Bakery on Biscotto" style="max-height: 60px; width: auto;">
+<td class="header" style="padding: 0;">
+<a href="{{ $url }}" style="display: block;">
+<img src="{{ asset('images/hero-banner.jpg') }}" alt="Bakery on Biscotto" style="width: 100%; max-width: 570px; height: auto; display: block;">
 </a>
 </td>
 </tr>
