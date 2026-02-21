42b8f20cbf765851a465bb0f3d477593c0f737bb
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-21 18:43:33 +0000

    Custom email header (logo) and footer (branding)

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('images/logo.jpg') }}" alt="Bakery on Biscotto" style="max-height: 60px; width: auto;">
+</a>
+</td>
+</tr>
