e7292ff1b78327dbdf085b96c7caf793e16339c7
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-22 02:27:52 +0000

    Add About link back to navigation

--- resources/views/components/main-nav.blade.php
+++ resources/views/components/main-nav.blade.php
@@ -9,6 +9,7 @@
 
 <nav class="main-nav" aria-label="Main navigation">
     <a href="{{ $isHome ? '#home' : '/' }}" @class(['active' => $active === 'home'])>Home</a>
+    <a href="/about" @class(['active' => $active === 'about'])>About</a>
     <a href="/menu" @class(['active' => $active === 'menu'])>Menu</a>
     <a href="/order" @class(['active' => $active === 'order'])>Order</a>
     <a href="/contact" @class(['active' => $active === 'contact'])>Contact</a>
