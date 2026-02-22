28a6c8e21a040b88426e4649fec11dbaf2171870
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-22 16:28:15 +0000

    feat: add FAQ to navigation

--- resources/views/components/main-nav.blade.php
+++ resources/views/components/main-nav.blade.php
@@ -12,6 +12,7 @@
     <a href="/about" @class(['active' => $active === 'about'])>About</a>
     <a href="/menu" @class(['active' => $active === 'menu'])>Menu</a>
     <a href="/gallery" @class(['active' => $active === 'gallery'])>Gallery</a>
+    <a href="/faq" @class(['active' => $active === 'faq'])>FAQ</a>
     <a href="/order" @class(['active' => $active === 'order'])>Order</a>
     <a href="/contact" @class(['active' => $active === 'contact'])>Contact</a>
 </nav>
