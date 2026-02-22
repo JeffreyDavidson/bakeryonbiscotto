b418bebfe3ab9bdc93ee87cd26a201332c7eb058
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-22 02:31:26 +0000

    Add Gallery link to navigation

--- resources/views/components/main-nav.blade.php
+++ resources/views/components/main-nav.blade.php
@@ -11,6 +11,7 @@
     <a href="{{ $isHome ? '#home' : '/' }}" @class(['active' => $active === 'home'])>Home</a>
     <a href="/about" @class(['active' => $active === 'about'])>About</a>
     <a href="/menu" @class(['active' => $active === 'menu'])>Menu</a>
+    <a href="/gallery" @class(['active' => $active === 'gallery'])>Gallery</a>
     <a href="/order" @class(['active' => $active === 'order'])>Order</a>
     <a href="/contact" @class(['active' => $active === 'contact'])>Contact</a>
 </nav>
