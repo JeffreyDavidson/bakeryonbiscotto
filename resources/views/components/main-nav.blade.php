13f4ef08a770e4d1a5d1172f79c57c8affe79172
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-22 01:50:17 +0000

    Remove About link from navigation

--- resources/views/components/main-nav.blade.php
+++ resources/views/components/main-nav.blade.php
@@ -9,7 +9,6 @@
 
 <nav class="main-nav" aria-label="Main navigation">
     <a href="{{ $isHome ? '#home' : '/' }}" @class(['active' => $active === 'home'])>Home</a>
-    <a href="{{ $prefix }}#about" @class(['active' => $active === 'about'])>About</a>
     <a href="/menu" @class(['active' => $active === 'menu'])>Menu</a>
     <a href="/order" @class(['active' => $active === 'order'])>Order</a>
     <a href="/contact" @class(['active' => $active === 'contact'])>Contact</a>
