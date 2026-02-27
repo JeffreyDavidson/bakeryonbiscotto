3b3b0509af057ad90e94858175fd6f5ea2654cb3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-27 02:51:57 +0000

    fix: constrain email banner to same 570px width as body content

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,14 @@
 @props(['url'])
 <tr>
-<td class="header" style="padding: 0;">
+<td class="header" style="padding: 0;" align="center">
+<table width="570" cellpadding="0" cellspacing="0" role="presentation" style="margin: 0 auto;">
+<tr>
+<td style="padding: 0;">
 <a href="{{ $url }}" style="display: block;">
-<img src="{{ asset('images/hero-banner.jpg') }}" alt="Bakery on Biscotto" style="width: 100%; max-width: 570px; height: auto; display: block;">
+<img src="{{ asset('images/hero-banner.jpg') }}" alt="Bakery on Biscotto" style="width: 100%; max-width: 570px; height: auto; display: block; border-radius: 8px 8px 0 0;">
 </a>
+</td>
+</tr>
+</table>
 </td>
 </tr>
