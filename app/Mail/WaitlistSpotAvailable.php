6e3e5a6f37a63fc6de40038b45b25ac7d2655384
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-14 17:03:14 +0000

    fix: add ShouldQueue to all mailables for async email delivery

--- app/Mail/WaitlistSpotAvailable.php
+++ app/Mail/WaitlistSpotAvailable.php
@@ -4,12 +4,13 @@ namespace App\Mail;
 
 use App\Models\WaitlistEntry;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class WaitlistSpotAvailable extends Mailable
+class WaitlistSpotAvailable extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
